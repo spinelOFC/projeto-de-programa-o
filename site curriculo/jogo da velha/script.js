7b464a721cad2cfb4cde0703a92edae8f7857c86
espinélioOFC <oarthurgamer.10@gmail.com>
2025-06-23 17:30:06 -0300

    jogo da velha

---
+++ site curriculo/jogo da velha/script.js
@@ -0,0 +1,19 @@
+var jogador = "X";
+
+function jogar(celula){
+//compra a celula  esta vazia para assim escrever
+    if(celula.innerHTML == ""){
+
+
+//escreva letra na celula
+celula.innerHTML = jogador;
+//altrna a variavel jogador para a proxima jogada
+if(jogador == "X"){
+    jogador = "O";
+    celula.style.backgroundColor = "red";
+}else{
+    jogador = "X";
+     celula.style.backgroundColor = "blue";
+}
+    }
+}
